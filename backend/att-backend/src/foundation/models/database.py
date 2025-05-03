3f818ac1f61bf6b20fa7dda2dee9861994c588bc
HaRo87 <github@fam-hansel.de>
2025-05-03 15:26:50 +0200

    fixing db connection

--- backend/att-backend/src/foundation/models/database.py
+++ backend/att-backend/src/foundation/models/database.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker, Session
 
 DEFAULT_DATABASE_URL = getenv(
-    "ATT_DB_URL", "postgresql+pg8000://docker:docker@127.0.0.1/docker"
+    "ATT_DB_URL",
+    "postgresql+pg8000://secret_user:secret_password@127.0.0.1/abc123",
 )
 
 
@@ -17,8 +18,12 @@ def get_sessionmaker(engine: Engine) -> sessionmaker[Session]:
     return sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db() -> Generator[sessionmaker[Session], None, None]:
-    db = get_sessionmaker(get_engine())
+engine = get_engine()
+LocalSession = get_sessionmaker(engine)
+
+
+def get_db() -> Generator[Session]:
+    db = LocalSession()
     try:
         yield db
     finally:
